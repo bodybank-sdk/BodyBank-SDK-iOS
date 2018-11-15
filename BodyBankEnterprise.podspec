c7a3247a030beac88d8bee9e81110998fdffb885
Shunpei Kobayashi <skonb@me.com>
2018-11-15 12:40:10 +0900

    bump version

--- BodyBankEnterprise.podspec
+++ BodyBankEnterprise.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
     s.name = 'BodyBankEnterprise'
     s.summary = 'BodyBank Enterprise iOS SDK'
-    s.version = '0.0.6'
+    s.version = '0.0.7'
     s.author = 'Original Inc.'
     s.license = 'Proprietary'
     s.homepage = 'https://originalstitch.com'
@@ -10,7 +10,7 @@ Pod::Spec.new do |s|
         :http => "https://github.com/skonb/BodyBankSDK/raw/master/BodyBankEnterprise.framework.zip"
     }
     s.ios.vendored_frameworks = 'BodyBankEnterprise.framework'
-    s.dependency 'AWSAppSync', '2.6.22'
-    s.dependency 'AWSS3', '2.6.32'
+    s.dependency 'AWSAppSync', '2.6.24'
+    s.dependency 'AWSS3', '2.7.1'
     s.dependency 'FCFileManager', '1.0.20'
 end
--- BodyBankEnterpriseDemo/BodyBankEnterpriseDemo/TokenProvider.swift
+++ BodyBankEnterpriseDemo/BodyBankEnterpriseDemo/TokenProvider.swift
@@ -12,7 +12,7 @@ import BodyBankEnterprise
 class TokenProvider: BodyBankTokenProvider{
     var token: BodyBankToken{
         get{
-            return BodyBankToken(jwtToken: "", identityId: "")
+            return BodyBankToken(jwtToken: "eyJraWQiOiJhcC1ub3J0aGVhc3QtMTEiLCJ0eXAiOiJKV1MiLCJhbGciOiJSUzUxMiJ9.eyJzdWIiOiJhcC1ub3J0aGVhc3QtMTo5NmIzYTRiNC1hYWY4LTRiMjUtYjFkMS1iN2FlZjlkNTg5YjQiLCJhdWQiOiJhcC1ub3J0aGVhc3QtMTo1YTMwMjdjOS0yNmIxLTRkNzItYTE0MC0wZmNjZDA1MWIzNGIiLCJhbXIiOlsiYXV0aGVudGljYXRlZCIsImNvbS5ib2R5YmFuay5lbnRlcnByaXNlLnNhbXBsZSIsImNvbS5ib2R5YmFuay5lbnRlcnByaXNlLnNhbXBsZTphcC1ub3J0aGVhc3QtMTo1YTMwMjdjOS0yNmIxLTRkNzItYTE0MC0wZmNjZDA1MWIzNGI6c2tvbmIrdGVzdEBtZS5jb20iXSwiaXNzIjoiaHR0cHM6Ly9jb2duaXRvLWlkZW50aXR5LmFtYXpvbmF3cy5jb20iLCJleHAiOjE1Mzk3NjYwOTUsImlhdCI6MTUzOTc2NTE5NX0.C46DAFtCumwXy8P-S9Oq2OYFMkHcaz8ZN6g5Cj2ki1KZU2he-k2nBLe0ho_zAVBpZhIWmVJjXHFTaj4L4iK6n8ng2ExJ_QVsdCPmMXqSE19flP9Sp3Hi6In4kn_Wry8AxoJmV5HDGVsAaHGtx4QaXbyYFvm7Mt85wzFX1ZtcNAlsxE87ehN27zRBS8_JMZzYcAithwNDcjTK7MHVns37SVEBjCugotI5LlQ5VRTVOPWubDaNoLFtCPGE5sFLAsly6cM32zjILd30sbiEmJRXGvrqK5T9nGQhC8BtpQAgjNFS-BH7IQgUMuZRw5pXRpgzuBCi_M7Zj2CTQ06OFQBPug", identityId: "ap-northeast-1:96b3a4b4-aaf8-4b25-b1d1-b7aef9d589b4")
         }
     }
 }
